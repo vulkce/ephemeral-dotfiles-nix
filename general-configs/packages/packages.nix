47ba7bf09394032237aa6f3ba289869e1b9fdec3
vulkce <vulkce@proton.me>
2025-12-15 04:41:31 -0300

    refactor: packages.nix

--- general-configs/packages/packages.nix
+++ general-configs/packages/packages.nix
@@ -38,58 +38,57 @@
 
 # -------- FLATPAK --------
 
-  services.flatpak.enable = true; # habilita a flatpak
-
-  # repositórios usados para instalar aplicativos
-  services.flatpak.remotes = [
-    { 
-      name = "flathub";
-      location = "https://flathub.org/repo/flathub.flatpakrepo"; 
-    }
-    {
-      name = "gnome-nightly"; 
-      location = "https://nightly.gnome.org/gnome-nightly.flatpakrepo"; 
-    } 
-    {
-      name = "elementaryos"; 
-      location = "https://flatpak.elementary.io/repo.flatpakrepo"; 
-    }  
-    { 
-      name = "PureOS"; 
-      location = "https://store.puri.sm/repo/stable/pureos.flatpakrepo"; 
-    }
-  ];
-  
-  # aplicativos declarados
-  services.flatpak.packages = [
-    { 
-      appId = "app.zen_browser.zen"; 
-      origin = "flathub"; 
-    }
-    { 
-      appId = "org.vinegarhq.Sober";
-      origin = "flathub";
-    }
-    {
-      appId = "net.newpipe.NewPipe";
-      origin = "flathub";
-    }
-    {
-      appId = "org.raspberrypi.rpi-imager";
-      origin = "flathub";
-    }
-    {
-      appId = "com.bitwarden.desktop";
-      origin = "flathub";
-    }
-  ];
-
-  # atualização automática:
-  services.flatpak.update = {
-    onActivation = true;
-    auto = {
-      enable = true;
-      onCalendar = "weekly";
+  services.flatpak = {
+    enable = true; # habilita a flatpak
+    # repositórios flatpak
+    remotes = [
+      { 
+        name = "flathub";
+        location = "https://flathub.org/repo/flathub.flatpakrepo"; 
+      }
+      {
+        name = "gnome-nightly"; 
+        location = "https://nightly.gnome.org/gnome-nightly.flatpakrepo"; 
+      } 
+      {
+        name = "elementaryos"; 
+        location = "https://flatpak.elementary.io/repo.flatpakrepo"; 
+      }  
+      { 
+        name = "PureOS"; 
+        location = "https://store.puri.sm/repo/stable/pureos.flatpakrepo"; 
+      }
+    ];
+    # aplicativos declarados
+    packages = [
+      { 
+        appId = "app.zen_browser.zen"; 
+        origin = "flathub"; 
+      }
+      { 
+        appId = "org.vinegarhq.Sober";
+        origin = "flathub";
+      }
+      {
+        appId = "net.newpipe.NewPipe";
+        origin = "flathub";
+      }
+      {
+        appId = "org.raspberrypi.rpi-imager";
+        origin = "flathub";
+      }
+      {
+        appId = "com.bitwarden.desktop";
+        origin = "flathub";
+      }
+    ];
+    # update automatico
+    update = {
+      onActivation = true;
+      auto = {
+        enable = true;
+        onCalendar = "weekly";
+      };
     };
   };
 }
